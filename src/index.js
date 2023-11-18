eb3532132c508df2da107f0b7cab207747923ab2
KrasiKirov <krasimir.kirov@mail.mcgill.ca>
2023-11-18 14:29:46 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
+import Layout from "./pages/Navbar";
 import Home from "./pages/Home";
 import Mathematics from "./pages/Mathematics";
 import Calculus1 from "./pages/Calculus1";
 import NoPage from "./pages/NoPage";
+import Navbar from "./pages/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<Navbar />}>
           <Route index element={<Home />} />
           <Route path="math" element={<Mathematics />}>
             <Route path="calculus1" element={<Calculus1 />} />
