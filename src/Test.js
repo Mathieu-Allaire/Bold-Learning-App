1c4eea5096e10f4c7328299d731c97a6abffdf3c
KrasiKirov <krasimir.kirov@mail.mcgill.ca>
2023-11-18 09:38:54 -0500

    Routes

---
+++ src/Test.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+
+
+function Test() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          TESTTTTTTT
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn to Test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Test;
