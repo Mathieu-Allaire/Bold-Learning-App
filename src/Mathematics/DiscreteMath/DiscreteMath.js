b82b5da4644bd15e158925cc02c7e5984a8181d1
Mathieu Allaire <mathieu.allaire2@mail.mcgill.ca>
2023-11-18 20:29:41 -0500

    Almost done

---
+++ src/Mathematics/DiscreteMath/DiscreteMath.js
@@ -0,0 +1,50 @@
+import '../Math.css';
+import { Helmet } from 'react-helmet';
+import { Link } from "react-router-dom";
+
+const DiscreteMath = () => {
+  return (
+    <div>
+        <Helmet>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+        </Helmet>
+        <div className="container text-center py-5">
+            <h1><span className="app-title">Discrete Math</span></h1>
+            <p>From simple number theory to complex discrete math.</p>
+            <p>Choose a level of difficulty</p>
+
+        </div>
+        <div className="features-container icon-container">
+
+        <Link to="/math/discreteMath/question"> 
+        
+        <div className="feature icon-difficulty">
+            <i class="fas fa-star"></i>
+                <h5>Easy question</h5>
+            </div>
+        </Link>
+        <Link to="/math/discreteMath/question">
+            <div className="feature icon-difficulty">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+                <h5>Medium question</h5>
+               
+            </div>
+        </Link>
+        <Link to="/math/discreteMath/question">
+            <div className="feature icon-difficulty">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+                <h5>Hard question</h5>
+            
+            </div>
+        </Link>
+        </div>
+    </div>
+);
+};
+  
+export default DiscreteMath;
